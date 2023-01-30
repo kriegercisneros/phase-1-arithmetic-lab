e29ad83ad5b7d990c8933949bdd819a16fae02df
JackieCisneros <krieger.jacqueline@gmail.com>
2023-01-30 16:14:53 -0700

    random, mod, max arithmic solutions

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 =2;
 
-// Write your code here
+let num2 = 31;
+
+let multiply =num1*num2
+
+let random = Math.floor(Math.random() * 100)
+
+let num3 = 24;
+
+let num4= 5;
+
+let mod = num3 % num4;
+let num5 = 20;
+
+let max = Math.max(num1, num4, num5);
